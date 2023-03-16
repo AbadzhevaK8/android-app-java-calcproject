9198172a1ef5caed3167a0762006390162906c5b
AbadzhevaK8 <make.me.loco@gmail.com>
2023-03-16 21:02:30 +0300

    feat: add fragments for all blocks.

---
+++ app/src/main/java/com/startandroid/calcproject/CalculateFragment.java
@@ -0,0 +1,19 @@
+package com.startandroid.calcproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CalculateFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_calculate, null);
+        return v;
+    }
+}
