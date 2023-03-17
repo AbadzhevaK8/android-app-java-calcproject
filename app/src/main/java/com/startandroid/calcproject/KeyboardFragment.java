ee3abf0f7e562dbbf1e889639359d72064589060
AbadzhevaK8 <make.me.loco@gmail.com>
2023-03-17 18:54:04 +0300

    feat: write KeyboardFragment.java content.

--- app/src/main/java/com/startandroid/calcproject/KeyboardFragment.java
+++ app/src/main/java/com/startandroid/calcproject/KeyboardFragment.java
@@ -7,58 +7,68 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link KeyboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class KeyboardFragment extends Fragment {
+public class KeyboardFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public KeyboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment KeyboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static KeyboardFragment newInstance(String param1, String param2) {
-        KeyboardFragment fragment = new KeyboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_keyboard, container, false);
+        View keyboardView = inflater.inflate(R.layout.fragment_keyboard,
+                container, false);
+
+        Button divisionBtn = keyboardView.findViewById(R.id.division);
+        Button multiplyBtn = keyboardView.findViewById(R.id.multiply);
+        Button subtractionBtn = keyboardView.findViewById(R.id.subtraction);
+        Button sumBtn = keyboardView.findViewById(R.id.sum);
+        Button sevenBtn = keyboardView.findViewById(R.id.seven);
+        Button eightBtn = keyboardView.findViewById(R.id.eight);
+        Button nineBtn = keyboardView.findViewById(R.id.nine);
+        Button rootBtn = keyboardView.findViewById(R.id.root);
+        Button fourBtn = keyboardView.findViewById(R.id.four);
+        Button fiveBtn = keyboardView.findViewById(R.id.five);
+        Button sixBtn = keyboardView.findViewById(R.id.six);
+        Button squareBtn = keyboardView.findViewById(R.id.square);
+        Button oneBtn = keyboardView.findViewById(R.id.one);
+        Button twoBtn = keyboardView.findViewById(R.id.two);
+        Button threeBtn = keyboardView.findViewById(R.id.three);
+        Button equalsBtn = keyboardView.findViewById(R.id.equals);
+        Button dotBtn = keyboardView.findViewById(R.id.dot);
+        Button zeroBtn = keyboardView.findViewById(R.id.zero);
+        Button percentBtn = keyboardView.findViewById(R.id.percent);
+        Button calcBtn = keyboardView.findViewById(R.id.calc);
+
+        divisionBtn.setOnClickListener(this);
+        multiplyBtn.setOnClickListener(this);
+        subtractionBtn.setOnClickListener(this);
+        sumBtn.setOnClickListener(this);
+        sevenBtn.setOnClickListener(this);
+        eightBtn.setOnClickListener(this);
+        nineBtn.setOnClickListener(this);
+        rootBtn.setOnClickListener(this);
+        fourBtn.setOnClickListener(this);
+        fiveBtn.setOnClickListener(this);
+        sixBtn.setOnClickListener(this);
+        squareBtn.setOnClickListener(this);
+        oneBtn.setOnClickListener(this);
+        twoBtn.setOnClickListener(this);
+        threeBtn.setOnClickListener(this);
+        equalsBtn.setOnClickListener(this);
+        dotBtn.setOnClickListener(this);
+        zeroBtn.setOnClickListener(this);
+        percentBtn.setOnClickListener(this);
+        calcBtn.setOnClickListener(this);
+
+        return keyboardView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        String buttonId = getResources().getResourceEntryName(view.getId());
+        Toast.makeText(getActivity(), buttonId, Toast.LENGTH_SHORT).show();
     }
 }
