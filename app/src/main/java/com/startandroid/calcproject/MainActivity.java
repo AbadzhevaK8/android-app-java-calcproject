fe8546f13c99f7600878ab9e7a3759b2c6040468
AbadzhevaK8 <make.me.loco@gmail.com>
2023-04-06 00:32:05 +0300

    info: for call 2.

--- app/src/main/java/com/startandroid/calcproject/MainActivity.java
+++ app/src/main/java/com/startandroid/calcproject/MainActivity.java
@@ -3,13 +3,17 @@ package com.startandroid.calcproject;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewModel = new ViewModelProvider(this).get(SharedViewModel.class);
     }
 }
